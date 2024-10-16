a242925ef8b7bd3e19acbbc54ec9c8cf61fb480c
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2024-10-16 18:08:50 +0200

    Change start value of sorted input to 1
    Originally was 0

--- src/scripts/input/presets/functions/InputFunctionSorted.ts
+++ src/scripts/input/presets/functions/InputFunctionSorted.ts
@@ -7,11 +7,12 @@ export class InputFunctionSorted extends InputFunction {
 
 	public getArray(): number[] {
 		const length = this.lengthParameter.getValueNumberMandatory();
-		
+		const startValue = 1;
+
 		let result = new Array(length);
 
 		for (let i = 0; i < result.length; i++) {
-			result[i] = i;
+			result[i] = i + startValue;
 		}
 
 		return result;
