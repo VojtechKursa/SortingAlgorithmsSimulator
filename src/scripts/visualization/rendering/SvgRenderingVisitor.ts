4cb03b30b012083744bde28a3551ad129f238717
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2024-12-23 14:10:36 +0100

    Add basic variable rendering

--- src/scripts/visualization/rendering/SvgRenderingVisitor.ts
+++ src/scripts/visualization/rendering/SvgRenderingVisitor.ts
@@ -7,7 +7,18 @@ import { ColorSet } from "../ColorSet";
 import { codeHighlightClass, VariableWatchClasses } from "../CssInterface";
 import { RenderingVisitor } from "./RenderingVisitor";
 
+class Rectangle {
+	public constructor(
+		public readonly x: number,
+		public readonly y: number,
+		public readonly height: number,
+		public readonly width: number
+	) { }
+}
+
 export class SvgRenderingVisitor implements RenderingVisitor {
+	private readonly arrayElementLocations = new Array<Rectangle>();
+
 	public constructor(
 		public colorSet: ColorSet,
 		public readonly output: SimulatorOutputElements
@@ -21,6 +32,8 @@ export class SvgRenderingVisitor implements RenderingVisitor {
 		if (step instanceof StepResultArray) {
 			const parent = this.output.renderer;
 
+			this.arrayElementLocations.splice(0, this.arrayElementLocations.length);
+
 			let borderWidth = 2;
 
 			let boxSize = Math.min((parent.clientWidth / step.array.length) - borderWidth, parent.clientHeight - borderWidth);
@@ -30,7 +43,11 @@ export class SvgRenderingVisitor implements RenderingVisitor {
 			let resultBuilder = new Array<string>();
 
 			for (let i = 0; i < step.array.length; i++) {
-				resultBuilder.push(`<rect x="${leftOffset + i * boxSize}" y="${y}" width="${boxSize}" height="${boxSize}" stroke="black" stroke-width="${borderWidth}px" fill="${this.colorSet.get(step.highlights != null ? step.highlights.get(i) : undefined)}" />`);
+				const rectX = leftOffset + i * boxSize;
+				
+				this.arrayElementLocations.push(new Rectangle(rectX, y, boxSize, boxSize));
+
+				resultBuilder.push(`<rect x="${rectX}" y="${y}" width="${boxSize}" height="${boxSize}" stroke="black" stroke-width="${borderWidth}px" fill="${this.colorSet.get(step.highlights != null ? step.highlights.get(i) : undefined)}" />`);
 				resultBuilder.push(`<text x="${leftOffset + (i + 0.5) * boxSize}" y="${y + (boxSize / 2)}" color="black" alignment-baseline="central" text-anchor="middle">${step.array[i].value}</text>`)
 			}
 
@@ -99,15 +116,25 @@ export class SvgRenderingVisitor implements RenderingVisitor {
 
 	protected codeStep_drawVariables(step: CodeStepResult) {
 		const variableRenderer = this.output.renderer;
-		//variableRenderer.querySelectorAll(`.${RendererClasses.variableClass}`).forEach(child => variableRenderer.removeChild(child));
+		const variableClass = "variable";
+		const textSize = 16;
+		const margin = textSize / 2;
+
+		variableRenderer.querySelectorAll(`.${variableClass}`).forEach(child => variableRenderer.removeChild(child));
 
 		step.variables.filter(variable => variable.draw).forEach(variable => {
-			let div = document.createElement("div");
-			//div.classList.add(RendererClasses.variableClass);
-			div.style.gridColumn = variable.value + 1;
-			div.textContent = variable.name;
+			const elementLocation = this.arrayElementLocations[variable.value];
 
-			variableRenderer.appendChild(div);
+			const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+			text.textContent = variable.name;
+			text.classList.add(variableClass);
+			text.setAttribute("x", (elementLocation.x + (elementLocation.width / 2)).toString());
+			text.setAttribute("y", (elementLocation.y - margin).toString());
+			text.setAttribute("font-size", `${textSize}px`);
+			text.setAttribute("text-anchor", "middle");
+			text.setAttribute("alignment-baseline", "bottom");
+
+			variableRenderer.appendChild(text);
 		});
 	}
 }
