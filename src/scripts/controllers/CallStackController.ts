956db2f57f1854ecdc19cdf267679684b8d9877b
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2025-01-06 14:10:03 +0100

    Improve Call Stack's styling
    Now as a table instead of series of divs

--- src/scripts/controllers/CallStackController.ts
+++ src/scripts/controllers/CallStackController.ts
@@ -1,27 +1,53 @@
 import { CallStack, CallStackFreezed } from "../data/CallStack";
+import { hiddenClass } from "../visualization/CssInterface";
 
 export class CallStackController {
+	protected readonly tableBody: HTMLTableSectionElement;
+
 	public constructor(
-		public readonly callStackWrapper: HTMLDivElement
-	) { }
+		public readonly callStackWrapper: HTMLDivElement,
+		visible: boolean = false
+	) {
+		this.visible = visible;
+
+		const body = callStackWrapper.querySelector("tbody#call_stack_body") as HTMLTableSectionElement | null;
+		if (body == null)
+			throw new Error("Invalid call stack wrapper, doesn't contain 'tbody' element with id 'call_stack_body'");
+
+		this.tableBody = body;
+	}
+
+	public get visible(): boolean {
+		return !this.callStackWrapper.classList.contains(hiddenClass);
+	}
+	public set visible(visible: boolean) {
+		if (visible)
+			this.callStackWrapper.classList.remove(hiddenClass);
+		else
+			this.callStackWrapper.classList.add(hiddenClass);
+	}
 
 	public display(stack: CallStack | CallStackFreezed): void {
 		if (stack instanceof CallStack)
 			stack = stack.freeze();
 
-		this.callStackWrapper.textContent = "";
+		this.tableBody.textContent = "";
 
 		for (const level of stack) {
-			const div = document.createElement("div");
-			const variableBuilder = new Array<string>();
+			const functionNameCol = document.createElement("td");
+			functionNameCol.textContent = level.functionName;
 
-			level.variables.forEach(variable => {
-				variableBuilder.push(`${variable.name}=${variable.value}`);
-			});
+			const variablesCol = document.createElement("td");
+			const variablesTextBuilder = new Array<string>();
+			level.variables.forEach(variable => variablesTextBuilder.push(`${variable.name}=${variable.value}`));
+			variablesCol.textContent = variablesTextBuilder.join(" ");
 
-			div.textContent = `${level.functionName} - ${variableBuilder.join(" ")}`;
+			const row = document.createElement("tr");
 
-			this.callStackWrapper.appendChild(div);
+			row.appendChild(functionNameCol);
+			row.appendChild(variablesCol);
+
+			this.tableBody.appendChild(row);
 		}
 	}
 }
