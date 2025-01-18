bf783322aad362b898f6ac0e053adc365455b5c5
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2025-01-18 11:31:22 +0100

    Rewrite SVG rendering
    - Use SVG's viewBox property to avoid calculating the image to pixels
    - Group objects for easier manipulation
    - Center rendered array on the center of the boxes

--- src/scripts/visualization/rendering/SvgArrayRenderVisitor.ts
+++ src/scripts/visualization/rendering/SvgArrayRenderVisitor.ts
@@ -2,19 +2,10 @@ import { CodeStepResult } from "../../data/stepResults/CodeStepResult";
 import { FullStepResult } from "../../data/stepResults/FullStepResult";
 import { StepResultArray } from "../../data/stepResults/StepResultArray";
 import { ColorSet } from "../colors/ColorSet";
-import { RendererClasses } from "../CssInterface";
+import { RendererClasses, verticalLayoutBreakPoint } from "../CssInterface";
 import { SymbolicColor } from "../colors/SymbolicColor";
 import { VariableDrawInformation } from "../../data/Variable";
 import { StepDisplayVisitorWithColor } from "./StepDisplayVisitorWithColor";
-
-class Rectangle {
-	public constructor(
-		public readonly x: number,
-		public readonly y: number,
-		public readonly height: number,
-		public readonly width: number
-	) { }
-}
 
 class Point2D {
 	public constructor(
@@ -27,9 +18,55 @@ class Point2D {
 	}
 }
 
+class FontProperties {
+	public constructor(
+		public readonly fontSize: number,
+		public readonly strokeWidth: number
+	) { }
+}
+
+class ArrayRenderSettings {
+	public constructor(
+		public readonly boxSize: number = 10,
+		public readonly horizontalMargin: number = 10,
+		public readonly verticalMargin: number = 10,
+		public readonly borderWidth: number = 0.5,
+		public readonly fontMain: FontProperties = new FontProperties(4, 0.5),
+		public readonly fontIndex: FontProperties = new FontProperties(2.5, 0.5),
+		public readonly indexRightMargin: number = 1,
+		public readonly indexBottomMargin: number = 1
+	) { }
+}
+
+class VariableRenderSettings {
+	public constructor(
+		public readonly chevronWidth: number,
+		public readonly chevronHeight: number = chevronWidth / 2,
+		public readonly chevronMargin: number = chevronHeight / 2,
+		public readonly chevronStrokeWidth: number = 0.5,
+		public readonly textFont: FontProperties = new FontProperties(2.5, 0.1),
+		public readonly textMarginBottom: number = 1,
+		public readonly textMarginTop: number = 0
+	) { }
+}
+
 export class SvgArrayRenderVisitor extends StepDisplayVisitorWithColor {
-	private readonly arrayElementLocations = new Array<Rectangle>();
-	private boxSize = 0;
+	private readonly arraySettings = new ArrayRenderSettings();
+	private readonly variableSettings = new VariableRenderSettings(this.arraySettings.boxSize * 0.8);
+
+	private readonly oneVariableSpace = (
+		this.variableSettings.chevronMargin +
+		this.variableSettings.chevronHeight +
+		this.variableSettings.textMarginBottom +
+		this.variableSettings.textFont.fontSize +
+		this.variableSettings.textMarginTop
+	);
+
+	private currentArrayLength: number | undefined;
+
+	private readonly defaultMinY = -this.arraySettings.borderWidth / 2;
+
+
 
 	public constructor(
 		colorSet: ColorSet,
@@ -41,7 +78,14 @@ export class SvgArrayRenderVisitor extends StepDisplayVisitorWithColor {
 		super(colorSet, next);
 	}
 
-	protected override displayFullStepInternal(step: FullStepResult): void {
+
+
+	protected override displayFullStepInternal(step: FullStepResult, redraw: boolean): void {
+		if (redraw) {
+			this.adjustMargin();
+			return;
+		}
+
 		if (step instanceof StepResultArray) {
 			this.drawArray(step);
 		}
@@ -49,88 +93,111 @@ export class SvgArrayRenderVisitor extends StepDisplayVisitorWithColor {
 			throw new Error("Step result type not supported by this visitor.");
 	}
 
-	protected override displayCodeStepInternal(step: CodeStepResult): void {
+	protected override displayCodeStepInternal(step: CodeStepResult, redraw: boolean): void {
+		if (redraw) {
+			this.adjustMargin();
+			return;
+		}
+
 		this.drawVariables(step);
 	}
 
 	private drawArray(step: StepResultArray) {
 		const parent = this.output;
 
-		parent.querySelectorAll(`.${RendererClasses.elementClass},.${RendererClasses.elementValueClass},.${RendererClasses.elementIndexClass}`)
-			.forEach(element => element.remove());
+		parent.querySelectorAll(`.${RendererClasses.elementWrapperClass}`).forEach(element => element.remove());
 
-		this.arrayElementLocations.splice(0, this.arrayElementLocations.length);
+		//TODO: Set properties of SVG elements using CSS styling embedded into SVG
+		/*
+		const style = document.createElementNS("http://www.w3.org/2000/svg", "style");
 
-		let borderWidth = 2;
+		style.sheet?.insertRule("aaa {aa:aa;}");
+		--- or ---
+		style.appendChild(document.createTextNode("aaa {aa: aa;}"));
 
-		let boxSize = Math.min((parent.clientWidth / step.array.length) - borderWidth, parent.clientHeight - borderWidth);
-		this.boxSize = boxSize;
-		let y = (parent.clientHeight - boxSize) / 2;
-		let leftOffset = (parent.clientWidth - step.array.length * boxSize) / 2;
+		Perhaps a better approach is adding rules directly using text nodes as there isn't a nullability issue with sheet
+			and I'm writing the CSS directly anyway.
+		*/
 
-		let boxSizeStr = boxSize.toString();
+		this.currentArrayLength = step.array.length;
 
 		for (let i = 0; i < step.array.length; i++) {
 			const item = step.array[i];
 
-			const box = new Rectangle(leftOffset + i * boxSize, y, boxSize, boxSize);
-			this.arrayElementLocations.push(box);
+			const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+			group.classList.add(RendererClasses.elementWrapperClass);
+			group.id = `elem_${i}`;
+
+			const rectX = i * this.arraySettings.boxSize;
+			const rectY = 0;
 
 			const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-			rect.setAttribute("x", box.x.toString());
-			rect.setAttribute("y", y.toString());
-			rect.setAttribute("height", boxSizeStr);
-			rect.setAttribute("width", boxSizeStr);
+			rect.setAttribute("x", rectX.toString());
+			rect.setAttribute("y", rectY.toString());
+			rect.setAttribute("height", this.arraySettings.boxSize.toString());
+			rect.setAttribute("width", this.arraySettings.boxSize.toString());
 			rect.setAttribute("stroke", this.colorSet.get(SymbolicColor.Element_Border).toString());
-			rect.setAttribute("stroke-width", `${borderWidth}px`);
+			rect.setAttribute("stroke-width", `${this.arraySettings.borderWidth}px`);
 			rect.setAttribute("fill", this.colorSet.get(step.highlights != null ? step.highlights.get(i) : SymbolicColor.Element_Background).toString());
-			rect.classList.add(RendererClasses.elementClass);
+			rect.classList.add(RendererClasses.elementBoxClass);
 
 			const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-			text.setAttribute("x", (leftOffset + (i + 0.5) * boxSize).toString());
-			text.setAttribute("y", (y + (boxSize / 2)).toString());
+			text.setAttribute("x", (rectX + (this.arraySettings.boxSize / 2)).toString());
+			text.setAttribute("y", (rectY + (this.arraySettings.boxSize / 2)).toString());
+			text.setAttribute("font-size", `${this.arraySettings.fontMain.fontSize}px`);
+			text.setAttribute("stroke-width", `${this.arraySettings.fontMain.strokeWidth}px`);
 			text.setAttribute("color", this.colorSet.get(SymbolicColor.Element_Foreground).toString());
 			text.setAttribute("dominant-baseline", "central");
 			text.setAttribute("text-anchor", "middle");
 			text.textContent = item.value.toString();
 			text.classList.add(RendererClasses.elementValueClass);
 
-			parent.appendChild(rect);
-			parent.appendChild(text);
+			group.appendChild(rect);
+			group.appendChild(text);
 
 			if (item.index != null) {
-				const rightMargin = 4;
-				const bottomMargin = 4;
-
 				const index = document.createElementNS("http://www.w3.org/2000/svg", "text");
-				index.setAttribute("x", (box.x + box.width - rightMargin).toString());
-				index.setAttribute("y", (box.y + box.height - bottomMargin).toString());
+				index.setAttribute("x", (rectX + this.arraySettings.boxSize - this.arraySettings.indexRightMargin).toString());
+				index.setAttribute("y", (rectY + this.arraySettings.boxSize - this.arraySettings.indexBottomMargin).toString());
+				index.setAttribute("font-size", `${this.arraySettings.fontIndex.fontSize}px`);
+				index.setAttribute("stroke-width", `${this.arraySettings.fontIndex.strokeWidth}px`);
 				index.setAttribute("color", this.colorSet.get(SymbolicColor.Element_Foreground).toString());
 				index.setAttribute("dominant-baseline", "text-bottom");
 				index.setAttribute("text-anchor", "end");
 				index.textContent = item.index.toString();
 				index.classList.add(RendererClasses.elementIndexClass);
 
-				parent.appendChild(index);
+				group.appendChild(index);
 			}
+
+			parent.appendChild(group);
 		}
+
+		this.adjustViewBox(this.defaultMinY);
+		this.adjustMargin();
 	}
 
 	private drawVariables(step: CodeStepResult) {
 		const variableRenderer = this.output;
 
-		variableRenderer.querySelectorAll(`.${RendererClasses.variableWrapperClass}`).forEach(child => variableRenderer.removeChild(child));
+		variableRenderer.querySelectorAll(`.${RendererClasses.variableWrapperClass}`).forEach(element => element.remove());
 
-		// temp
-		variableRenderer.querySelectorAll(`.${RendererClasses.variableTextClass}`).forEach(child => variableRenderer.removeChild(child));
-		variableRenderer.querySelectorAll(`.${RendererClasses.variablePointerClass}`).forEach(child => variableRenderer.removeChild(child));
+		if (this.currentArrayLength == undefined)
+			throw new Error("Attempted to draw variables without drawing an array first");
 
-		const variablesAboveElements = new Array<number>(this.arrayElementLocations.length);
+		const variablesAboveElements = new Array<number>(this.currentArrayLength);
+		let minY = this.defaultMinY;
 
 		step.variables.forEach(variable => {
 			const drawInformation = variable.getDrawInformation();
-			if (drawInformation != null)
-				this.drawVariable(drawInformation, variablesAboveElements, variableRenderer, 1);
+
+			if (drawInformation != null) {
+				const variableMinY = this.drawVariable(drawInformation, variablesAboveElements, variableRenderer, 1);
+
+				if (variableMinY != null && variableMinY < minY) {
+					minY = variableMinY;
+				}
+			}
 		});
 
 		if (this.drawLastStackLevelVariables && step.callStack != undefined) {
@@ -139,57 +206,119 @@ export class SvgArrayRenderVisitor extends StepDisplayVisitorWithColor {
 			if (lastCallLevel != undefined) {
 				lastCallLevel.variables.forEach(variable => {
 					const drawInformation = variable.getDrawInformation();
-					if (drawInformation != null)
-						this.drawVariable(drawInformation, variablesAboveElements, variableRenderer, 0.5);
+
+					if (drawInformation != null) {
+						const variableMinY = this.drawVariable(drawInformation, variablesAboveElements, variableRenderer, 0.5);
+
+						if (variableMinY != null && variableMinY < minY) {
+							minY = variableMinY;
+						}
+					}
 				});
 			}
 		}
+
+		this.adjustViewBox(minY);
+		this.adjustMargin();
 	}
 
-	private drawVariable(variable: VariableDrawInformation, variablesAboveElements: number[], output: SVGSVGElement, alphaFactor: number = 1) {
-		if (variable.drawAtIndex == null || variable.drawAtIndex < 0 || variable.drawAtIndex >= this.arrayElementLocations.length)
-			return;
+	private drawVariable(variable: VariableDrawInformation, variablesAboveElements: number[], output: SVGSVGElement, alphaFactor: number = 1): number | null {
+		if (this.currentArrayLength == undefined)
+			throw new Error("Attempted to draw variable without drawing an array first");
 
-		const textSize = 16;
-		const chevronMargin = textSize / 2;
-		const textMargin = chevronMargin / 2;
+		if (variable.drawAtIndex == null || variable.drawAtIndex < -1 || variable.drawAtIndex > this.currentArrayLength)
+			return null;
 
-		const chevronWidth = this.boxSize;
-		const chevronHeight = this.boxSize / 2;
+		const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+		group.classList.add(RendererClasses.variableWrapperClass);
 
-		const elementLocation = this.arrayElementLocations[variable.drawAtIndex];
-		const variableOrder = variablesAboveElements[variable.drawAtIndex] ?? 0;
+		const variableIndex = variablesAboveElements[variable.drawAtIndex] ?? 0;
 
-		const chevronTop = elementLocation.y - chevronMargin - chevronHeight - (variableOrder * (chevronMargin + chevronHeight + (textMargin * 2) + textSize));
+		const chevronTop = -(variableIndex * this.oneVariableSpace) - this.variableSettings.chevronMargin - this.variableSettings.chevronHeight;
+		const chevronCenterX = (variable.drawAtIndex + 0.5) * this.arraySettings.boxSize;
+		const chevronX1 = chevronCenterX - (this.variableSettings.chevronWidth / 2);
+		const chevronX2 = chevronCenterX + (this.variableSettings.chevronWidth / 2);
 
 		const points = new Array<Point2D>();
-		points.push(new Point2D(elementLocation.x, chevronTop));
-		points.push(new Point2D(elementLocation.x + chevronWidth, chevronTop));
-		points.push(new Point2D(elementLocation.x + (chevronWidth / 2), chevronTop + chevronHeight));
+		points.push(new Point2D(chevronX1, chevronTop));
+		points.push(new Point2D(chevronX2, chevronTop));
+		points.push(new Point2D(chevronCenterX, chevronTop + this.variableSettings.chevronHeight));
 
 		const chevron = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
 		chevron.classList.add(RendererClasses.variablePointerClass);
 		chevron.setAttribute("points", points.map(point => point.toString()).join(" "));
 		chevron.setAttribute("stroke", "black");
-		chevron.setAttribute("stroke-width", "1");
+		chevron.setAttribute("stroke-width", this.variableSettings.chevronStrokeWidth.toString());
 
 		const color = this.colorSet.get(variable.color).clone();
 		color.alpha *= alphaFactor;
 		chevron.setAttribute("fill", color.toString());
 
+		const chevronBorderTop = chevronTop - (this.variableSettings.chevronStrokeWidth / 2);
+
 		const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
 		text.textContent = variable.variableName;
 		text.classList.add(RendererClasses.variableTextClass);
-		text.setAttribute("x", (elementLocation.x + (elementLocation.width / 2)).toString());
-		text.setAttribute("y", (chevronTop - textMargin).toString());
-		text.setAttribute("font-size", `${textSize}px`);
-		text.setAttribute("text-anchor", "middle");
-		text.setAttribute("alignment-baseline", "bottom");
+		text.setAttribute("x", chevronCenterX.toString());
+		text.setAttribute("y", (chevronBorderTop - this.variableSettings.textMarginBottom).toString());
+		text.setAttribute("font-size", `${this.variableSettings.textFont.fontSize}px`);
+		text.setAttribute("stroke-width", `${this.variableSettings.textFont.strokeWidth}px`);
 		text.setAttribute("color", this.colorSet.get(SymbolicColor.Simulator_Foreground).toString());
+		text.setAttribute("text-anchor", "middle");
+		text.setAttribute("dominant-baseline", "text-bottom");
 
-		output.appendChild(chevron);
-		output.appendChild(text);
+		group.appendChild(chevron);
+		group.appendChild(text);
 
-		variablesAboveElements[variable.drawAtIndex] = variableOrder + 1;
+		output.appendChild(group);
+
+		variablesAboveElements[variable.drawAtIndex] = variableIndex + 1;
+
+		return chevronBorderTop - this.variableSettings.textMarginBottom - this.variableSettings.textFont.fontSize - this.variableSettings.textMarginTop;
+	}
+
+	private adjustViewBox(minY: number): boolean {
+		if (this.currentArrayLength == undefined)
+			return false;
+
+		const startX = -this.arraySettings.horizontalMargin;
+		const endX = (this.currentArrayLength * this.arraySettings.boxSize) + this.arraySettings.horizontalMargin;
+
+		const startY = minY;
+		const endY = this.arraySettings.boxSize + (this.arraySettings.borderWidth / 2);
+
+		const width = endX - startX;
+		const height = endY - startY;
+
+		this.output.setAttribute("viewBox", `${startX} ${startY} ${width} ${height}`);
+		return true;
+	}
+
+	/**
+	 * Centers the SVG inside the parent container so the array boxes are always in the same place (if possible),
+	 * even when stacking variables increase the height of the SVG element.
+	*/
+	private adjustMargin(): void {
+		if (window.innerWidth < verticalLayoutBreakPoint) {
+			this.output.style.marginTop = "";
+			return;
+		}
+
+		const parentHeight = this.output.parentElement?.clientHeight;
+		if (parentHeight == undefined)
+			return;
+
+		const svgHeight = this.output.clientHeight;
+		const viewBoxHeight = this.output.viewBox.baseVal.height;
+
+		const pixelToUnitRatio = svgHeight / viewBoxHeight;
+		const targetLine = (this.arraySettings.boxSize + this.arraySettings.borderWidth) / 2;
+
+		const margin = (parentHeight / 2) - svgHeight + (targetLine * pixelToUnitRatio);
+
+		// Prevents clipping of the SVG above it's parent
+		if (margin >= 0) {
+			this.output.style.marginTop = `${margin}px`;
+		}
 	}
 }
