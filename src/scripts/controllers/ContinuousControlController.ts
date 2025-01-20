a0323e7cdf78e95467b27137d541eb7e6d08321d
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2025-01-20 18:51:11 +0100

    Add bootstrap classes to continuous controls

--- src/scripts/controllers/ContinuousControlController.ts
+++ src/scripts/controllers/ContinuousControlController.ts
@@ -20,10 +20,12 @@ export class ContinuousControlController {
 			radio.name = "continuous_control-step_kind";
 			radio.value = kind.machineName;
 			radio.id = `input-continuous_control-step_kind-${kind.machineName}`;
+			radio.classList.add("form-check-input");
 
 			const label = document.createElement("label");
 			label.setAttribute("for", radio.id);
 			label.textContent = kind.displayName;
+			label.classList.add("form-check-label");
 
 			const div = document.createElement("div");
 			div.appendChild(radio);
