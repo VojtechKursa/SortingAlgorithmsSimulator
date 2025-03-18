3c9c491c86009b0b1fcd5c10e85f20a0a4ab5121
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2025-03-18 14:08:54 +0100

    Add basic generation of algorithm description dialog without properties

--- src/scripts/controllers/AlgorithmDescriptionController.ts
+++ src/scripts/controllers/AlgorithmDescriptionController.ts
@@ -1,5 +1,6 @@
 import { SortFamilyProperties } from "../../sortsConfigs/definitions/SortFamilyProperties";
 import { SortProperties } from "../../sortsConfigs/definitions/SortProperties";
+import { AlgorithmDescriptionClasses } from "../visualization/css/AlgorithmDescriptionClasses";
 
 export class AlgorithmDescriptionController {
 	private readonly dialog: HTMLDialogElement;
@@ -8,11 +9,45 @@ export class AlgorithmDescriptionController {
 		private readonly properties: SortFamilyProperties | SortProperties
 	) {
 		this.dialog = document.createElement("dialog");
+		this.dialog.classList.add(AlgorithmDescriptionClasses.dialogClass);
+
+		this.generateDialog();
 
 		this.dialog.addEventListener("close", () => {
 			document.body.classList.remove("blur");
 			document.body.removeChild(this.dialog);
 		});
+	}
+
+	private generateDialog(): void {
+		this.dialog.innerHTML = "";
+
+		const header = document.createElement("h2");
+		header.textContent = this.properties.name;
+
+		const propertiesWrapper = this.generateProperties();
+
+		const description = document.createElement("div");
+		description.classList.add(AlgorithmDescriptionClasses.descriptionClass);
+		description.textContent = this.properties.longDescription;
+
+		const closeButton = document.createElement("button");
+		closeButton.textContent="Close";
+		closeButton.classList.add("btn", "btn-primary");
+
+		this.dialog.appendChild(header);
+		this.dialog.appendChild(propertiesWrapper);
+		this.dialog.appendChild(description);
+		this.dialog.appendChild(closeButton);
+
+		closeButton.addEventListener("click", () => this.close());
+	}
+
+	private generateProperties(): HTMLDivElement {
+		const propertiesWrapper = document.createElement("div");
+		propertiesWrapper.classList.add(AlgorithmDescriptionClasses.propertiesClass);
+
+		return propertiesWrapper;
 	}
 
 	public open(): void {
---
+++ src/scripts/visualization/css/AlgorithmDescriptionClasses.ts
@@ -0,0 +1,5 @@
+export class AlgorithmDescriptionClasses {
+	public static readonly dialogClass = "algorithm_description-dialog";
+	public static readonly propertiesClass = "properties";
+	public static readonly descriptionClass = "description";
+}
